814e08503c8fe47578f4946f781830c3f1f2e69c
Gustavo Henrique <ymooncake@gmail.com>
2022-07-13 17:34:16 -0300

    update archives(13/07/2022)
    Arrumei umas configuraçõeszinhas e mudei o readme

--- project/controller/app.py
+++ project/controller/app.py
@@ -1,15 +1,17 @@
 from cgitb import html
-from flask import Flask
-
+from flask import Flask, render_template
 
 # instância da classe flask
 app = Flask(__name__)
 
-#Rotas do aplicativo com decorator
+# Rotas do aplicativo com decorator
+
+
 @app.route("/")
 def index():
-    return 'Hello, world'
+    return render_template('index.html')
 
-#Checando se o usuário está acessando o programa principal
+
+# Checando se o usuário está acessando o programa principal
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug=True)
