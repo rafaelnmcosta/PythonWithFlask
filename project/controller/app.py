a38f6e7de460aaaa8d70b900f4fc83ccdd8e75c7
Gustavo Henrique <ymooncake@gmail.com>
2022-07-13 23:47:14 -0300

    Working Update (13/07/2022)
    Agr funcionou, tava faltando colocar template folder ali

--- project/controller/app.py
+++ project/controller/app.py
@@ -2,12 +2,12 @@ from cgitb import html
 from flask import Flask, render_template
 
 # instância da classe flask
-app = Flask(__name__)
+app = Flask(__name__,template_folder='../template')
 
 # Rotas do aplicativo com decorator
 @app.route("/")
 def index():
-    return "<h1>Hello World!</h1>"
+    return render_template ('index.html')
 
 
 # Checando se o usuário está acessando o programa principal
