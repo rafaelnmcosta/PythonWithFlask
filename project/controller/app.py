a2d469d5353fda5e9e0d1cf48824402870561675
Gustavo Henrique <ymooncake@gmail.com>
2022-07-14 20:10:31 -0300

    Atualização (14/07/2022)
    Criação da função que vai fazer upload das imagens e reestruturação de alguns arquivos

--- project/controller/app.py
+++ project/controller/app.py
@@ -1,13 +1,28 @@
 from cgitb import html
+import os
+
+from aiohttp import request
+from werkzeug.utils import secure_filename
 from flask import Flask, render_template
 
 # instância da classe flask
-app = Flask(__name__,template_folder='../template')
+app = Flask(__name__, template_folder='../template')
+
+directory = os.path.join(os.getcwd(), 'upload')
 
 # Rotas do aplicativo com decorator
 @app.route("/")
 def index():
-    return render_template ('index.html')
+    return render_template('index.html')
+
+
+# Rota que faz upload da imagem
+@app.route("/upload", methods=['POST'])
+def uploaded():
+    file = request.files['imagem']
+    savePath = os.path.join(directory, secure_filename(file.filename))
+    file.save(savePath)
+    return 'uploded'
 
 
 # Checando se o usuário está acessando o programa principal
